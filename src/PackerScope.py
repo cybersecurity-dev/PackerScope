33b4f38e59148e0f46d3769fd923ac483e6562a5
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2024-10-29 21:07:51 +0100

    Updated PackerScope.py
    recursively traverse directories code added

--- src/PackerScope.py
+++ src/PackerScope.py
@@ -21,33 +21,34 @@ def csv_to_pkl(csv_file, pkl_file):
 
 def analyze_executables(input_folder, output_csv):
     # Create a list to store the results
-    results = []
-
-    # Iterate through each file in the folder
-    for filename in os.listdir(input_folder):
-        file_path = os.path.join(input_folder, filename)
-        print(file_path)
-
-        # Check if it's a file (not a directory)
-        if os.path.isfile(file_path):
-            try:
-                # Run the diec command on the file
-                command = f'diec -rd --heuristicscan --verbose "{file_path}"'
-                output = subprocess.run(command, shell=True, capture_output=True, text=True)
-
-                # Parse the output to determine if the file is packed and identify the packer used
-                packed_status = 'Not Packed'
-                packer_name = 'None'
-                if 'packer' in output.stdout.lower():
-                    packed_status = 'Packed'
-                    packer_name = output.stdout.split('Packer:')[1].split('\n')[0].strip()
-                file_sha256_hash = get_sha256_hash(file_path)
-                # Append the results (SHA256 File Checksum, filename, packed status, packer name) to the list
-                results.append([file_sha256_hash, filename, packed_status, packer_name])
+    results = []   
             
-            except Exception as e:
-                # In case of an error, append the filename with an error message
-                results.append([filename, 'Error', str(e)])
+    # Iterate through each file in the folder
+    for root, dirs, files in os.walk(input_folder):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            # Check if it's a file (not a directory)
+            if os.path.isfile(file_path):
+                try:
+                    print(file_path)
+                    # Run the diec command on the file
+                    command = f'diec -rd --heuristicscan --verbose "{file_path}"'
+                    output = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+                    # Parse the output to determine if the file is packed and identify the packer used
+                    packed_status = 'Not Packed'
+                    packer_name = 'None'
+                    if 'packer' in output.stdout.lower():
+                        packed_status = 'Packed'
+                        packer_name = output.stdout.split('Packer:')[1].split('\n')[0].strip()
+                    file_sha256_hash = get_sha256_hash(file_path)
+                    # Append the results (SHA256 File Checksum, filename, packed status, packer name) to the list
+                    results.append([file_sha256_hash, filename, packed_status, packer_name])
+                
+                except Exception as e:
+                    # In case of an error, append the filename with an error message
+                    print(f"Error:{file_path}:\t{str(e)}" )
+                    results.append([file_path, 'Error', str(e)])
 
     # Save the results to a CSV file
     with open(output_csv, 'w', newline='') as csvfile:
